9d6ca8c1ab01d923e513be1fd2fdc81374882f58
soloscribblecreatives <34957574+soloscribblecreatives@users.noreply.github.com>
2023-07-03 04:07:03 +0530

    spin

--- global.js
+++ global.js
@@ -12,8 +12,8 @@ var currentContentNSlide ='';
 //alert("++++++++++++"+custcomslideflag1+"+++++++custcomslideid+++++++"+custcomslideid1);
 	if (typeof(localStorage.getItem("currentcustomslideflag"))!='undefined' &&  localStorage.getItem("currentcustomslideflag") =='true'){
 		var custcomslideid1=parseInt(localStorage.getItem("currentcontentcustomslideId"));
+		
 		//step 2:
-
 		currentContentNSlide = currentContentId+"_"+contentName+"_"+custcomslideid1;
 		//step 2 ends here
 		localStorage.setItem("current",currentContentNSlide);
@@ -99,6 +99,7 @@ function touchHandler(e) {
 		console.log("swipeleft"+localStorage.getItem("currentslide"));
 		localStorage.setItem("previousslide",localStorage.getItem("currentslide"));
 		//step 4 ends here
+
 		//alert("swipeleft");
 		//myconsole("swipeleft");
 		var page_id =  parseInt($("#wrapper").attr("rel"));
@@ -116,6 +117,7 @@ function touchHandler(e) {
 		console.log("swiperight"+localStorage.getItem("currentslide"));
 		localStorage.setItem("previousslide",localStorage.getItem("currentslide"));
 		//step 5 ends here 
+
 			//alert("swiperight");
 		//myconsole("swiperight");
 		var page_id =  parseInt($("#wrapper").attr("rel"));
@@ -134,7 +136,10 @@ function touchHandler(e) {
         //Default is 75px, set to 0 for demo so any distance triggers swipe
          threshold:0
 	});
+
+
 });
+
 
 //step 6:-
 function toCaptureTime(page_id){
@@ -149,39 +154,31 @@ function toCaptureTime(page_id){
 	if(temp == null){
 		
 		if (currentSlideNo!=0){
+
+
+
 			localStorage.setItem(currentContentId+"_"+contentName+"_slideNo_"+currentSlideNo ,startTime);
 
 			//to capture start time of slide in db format
 			var startTimeInDBFormat = currentTimeInDatabaseFormat();
 			//alert(startTimeInDBFormat);
 
+
+
+
 			localStorage.setItem(currentContentId+"_"+contentName+"_StartTime_"+currentSlideNo ,startTimeInDBFormat);
 		}
 }
 else
 {
-
 var existingTime = localStorage.getItem(currentContentId+"_"+contentName+"_slideNo_"+currentSlideNo);
 var newTime = Date.now();
 var newSlideTime = (newTime - existingTime);
-
-// alert(currentSlideNo + " slide time : " + (newSlideTime/1000) ); // time taken to view that particular slide
-
-//to capture end time of slide in db format
 var endTimeInDBFormat = currentTimeInDatabaseFormat();
-//alert(endTimeInDBFormat);/* 
-/* 
-alert('do calculations and update time====else==_EndTime_======'); */
-
-
 var EndTimeNext = localStorage.getItem(currentContentId+"_"+contentName+"_EndTime_"+currentSlideNo);
 console.log("++++++++EndTimeNext++++++++"+EndTimeNext+"++++++currentContentId+++"+currentContentId+"_"+contentName+"_EndTime_"+currentSlideNo);
 if(EndTimeNext == null){
-	//this time for which the slide was viewed
-
-
 localStorage.setItem(currentContentId+"_"+contentName+"_totalTime_slideNo_"+currentSlideNo ,(newSlideTime/1000) );
-
 
 localStorage.setItem(currentContentId+"_"+contentName+"_EndTime_"+currentSlideNo ,endTimeInDBFormat);
 }
@@ -193,12 +190,10 @@ if (typeof(localStorage.getItem('currentslide'))!='undefined' && localStorage.ge
 
 }else{
 
-
 	var nextSlideNo = currentSlideNo + 1 ;
 	
  } 
- 
-	if(nextSlideNo <= 3){//number 3 is number of total slides present
+	if(nextSlideNo <= 1){//number 3 is number of total slides present
 	// alert(nextSlideNo);
 	var tempNext = localStorage.getItem(currentContentId+"_"+contentName+"_slideNo_"+nextSlideNo);
 
@@ -208,38 +203,28 @@ if (typeof(localStorage.getItem('currentslide'))!='undefined' && localStorage.ge
 				var nextSlideStartTime =  Date.now();
 				localStorage.setItem(currentContentId+"_"+contentName+"_slideNo_"+nextSlideNo ,nextSlideStartTime);
 				localStorage.setItem(currentContentId+"_"+contentName+"_totalTime_slideNo_"+nextSlideNo ,0);
-
-
-
 				//to capture start time of next slide in db format
 				var startTimeNextInDBFormat = currentTimeInDatabaseFormat();
-				//alert(startTimeNextInDBFormat);
-				/* 
-				alert("+++else++_StartTime_++++currentSlideNo++++"+nextSlideNo); */
 				localStorage.setItem(currentContentId+"_"+contentName+"_StartTime_"+nextSlideNo ,startTimeNextInDBFormat);
 			}
 		}
 	}
 }
 
+
 }
-//step ends..
+//step 6 ends here ..
 
 
 function go_nav(direction) {
-	//alert("+++++++++go_nav++++++++++"+direction);
-	//alert("+++++++++go_nav++++contentName++++++"+contentName);
-	//go_nav('b');right
-	//go_nav('f');left
+
 	
 //custom slide changes continues here....
 	
 	if (typeof(localStorage.getItem("currentcustomslideflag"))!='undefined' &&   localStorage.getItem("currentcustomslideflag") =='true'){
 
-			
 				var custcomslideid=parseInt(localStorage.getItem("currentcontentcustomslideId"));
 			
-				
 				var page_id =  custcomslideid;
 		}else{
 			
@@ -252,7 +237,7 @@ function go_nav(direction) {
 	//toCaptureTime(page_id);
 	console.log("swipeleft"+localStorage.getItem("currentslide"));
 	localStorage.setItem("previousslide",localStorage.getItem("currentslide"));
-	//step 7 ends here
+	//step 7 ends here 
 //localStorage.setItem(contentName+"_slideNo_"+currentSlideNo ,n);
 var flag=0;
 if(direction == 'b') {
@@ -266,28 +251,18 @@ if(direction == 'b') {
 		window.location = "js-call:" + "1" + ":" + encodeURIComponent(JSON.stringify({query:'NODATA', type:'brandNavigation', callback:'checkLastPgFn'}));
 
 
-	}
+	}else{
+	if(page_id == 1){
+	localStorage.setItem("gotoNextPrevBrand" ,2);//if one than next if 2 than prev
+	window.location = "js-call:" + "1" + ":" + encodeURIComponent(JSON.stringify({query:'NODATA', type:'brandNavigation', callback:'checkLastPgFn'}));
+
+
+}else{
+	localStorage.setItem("gotoNextPrevBrand" ,0);//if one than next if 2 than prev
+}
+}
 	
 //custom slide changes ends here....
-
-	else{
-	if(page_id >= 0){
-		page_id = page_id - 1;
-		//alert(page_id);
-		//console.log(page_id);
-		if(page_id == 0){
-            flag=2;
-        }
-	}
-	 if(flag == 2){
-        localStorage.setItem("gotoNextPrevBrand" ,2);//if one than next if 2 than prev
-        //flag == 0;
-		window.location = "js-call:" + "1" + ":" + encodeURIComponent(JSON.stringify({query:'NODATA', type:'brandNavigation', callback:'checkLastPgFn'}));
-    }else{
-        localStorage.setItem("gotoNextPrevBrand" ,0);
-    }
-
-	}
 }else {
 	
 //custom slide changes continues here....
@@ -300,28 +275,21 @@ if(direction == 'b') {
 	}
 	
 //custom slide changes ends here....
+else{
+	if(page_id == 1){
+	 localStorage.setItem("gotoNextPrevBrand" ,1);//if one than next if 2 than prev
+	 window.location = "js-call:" + "1" + ":" + encodeURIComponent(JSON.stringify({query:'NODATA', type:'brandNavigation', callback:'checkLastPgFn'}));
+}else{
 
-	else{
-	if(page_id <= 3){
-		page_id = page_id + 1;
-		//alert(page_id);
-		if(page_id == 4){
-            flag=1;
-        }
+
+
+	localStorage.setItem("gotoNextPrevBrand" ,0);//if one than next if 2 than prev
+}
 	}
-	    if(flag == 1){
-        localStorage.setItem("gotoNextPrevBrand" ,1);//if one than next if 2 than prev
-         flag == 0;
-		 window.location = "js-call:" + "1" + ":" + encodeURIComponent(JSON.stringify({query:'NODATA', type:'brandNavigation', callback:'checkLastPgFn'}));
-    }else{
-        localStorage.setItem("gotoNextPrevBrand" ,0);
-    }
-
 }
-}
-
 
 //step 8:
+
 currentContentNSlide = currentContentId+"_"+contentName+"_"+page_id;
 //step 8 ends here
 localStorage.setItem("current",currentContentNSlide);
@@ -364,10 +332,7 @@ var pg_content = set_pg_content(page_id);
 }
 
 function set_pg_content(pg_id){
-//step 6:-
-//console.log("++++++++pg_id++++"+pg_id+"+++++++currentslide++++++"+localStorage.getItem("currentslide")+"++++++previousslide++++++"+localStorage.getItem("previousslide"));
-		 //check previous slide id end time capture...@bramha..
-		 //check previous slide id end time capture...@bramha..
+//alert("++++++++++set_pg_content++++++++++"+pg_id);
 	//step 9:
 	if (typeof(localStorage.getItem("previousslide"))!='undefined'){
 		//to checked previous slide has god end time...
@@ -376,23 +341,15 @@ function set_pg_content(pg_id){
 		
 	}
 	toCaptureTime(pg_id);
-
-
 	//step 9 ends here..
-//alert("++++++++++set_pg_content++++++++++"+pg_id);
 $(".reference").removeClass("active");
 currentSlide();
 var selectedContentPath='';
 switch(pg_id){
 	case 1:
-	content='<link rel="stylesheet" type="text/css" href="slide1/slide2.css" media="screen"/><div class="background"><img src="slide1/1.jpg" width="1024" height="768" alt=""></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="background"><img src="slide1/1.jpg" width="1024" height="768"></div><div class="button1" onclick="playVid1()"></div><div class="video1"><video onended="myFunction()" id="startVideo" width="1024" height="768"><source src="slide1/spin1.mp4" type="video/mp4"></video></div><div class="button2" onclick="playVid2()"><img src="slide1/Restore.png"></div><div class="video2"><video onended="myFunction1()" id="endVideo" width="1024" height="768"><source src="slide1/spin2.mp4" type="video/mp4"></video></div><audio id="success" src="slide3/Success.mp3" type="audio/mpeg"></audio><div class="success"><img src="slide1/Success.png" width="1024" height="768"></div>';
 	break;
-	case 2:
-	content='<link rel="stylesheet" type="text/css" href="slide1/slide2.css" media="screen"/><div class="background"><img src="slide2/1.jpg" width="1024" height="768" alt=""></div>';
-	break;
-	case 3:
-	content='<link rel="stylesheet" type="text/css" href="slide3/slide3.css" media="screen"/><div class="background"><img src="slide3/1.jpg" width="1024" height="768" alt=""></div><div class="score" onclick="score()"><img src="slide3/2.jpg" width="1024" height="768" alt=""></div><div class="fact" onclick="fact()"><img src="slide3/3.jpg" width="1024" height="768" alt=""></div><div class="yoga" onclick="yoga()"><img src="slide3/4.jpg" width="1024" height="768" alt=""></div><div class="telma" onclick="telma()"><img src="slide3/5.jpg" width="1024" height="768" alt=""></div><div class="halsanaHit" onclick="halsanaHit()"></div><div class="halsana"><img src="slide3/Halsana.png"></div><div class="virasanaHit" onclick="virasanaHit()"></div><div class="virasana"><img src="slide3/Virasana.png"></div><div class="savasanaHit" onclick="savasanaHit()"></div><div class="savasana"><img src="slide3/Savasana.png"></div><div class="balasanaHit" onclick="balasanaHit()"></div><div class="balasana"><img src="slide3/Balasana.png"></div><div class="konasanaHit" onclick="konasanaHit()"></div><div class="konasana"><img src="slide3/Konasana.png"></div><video id="halsana" onended="halsanaClose()" width="1024" height="768" src="slide3/Halsana.mp4" type="video/mp4"></video><video id="virasana" onended="virasanaClose()" width="1024" height="768" src="slide3/Virasana.mp4" type="video/mp4"></video><video id="savasana" onended="savasanaClose()" width="1024" height="768" src="slide3/Savasana.mp4" type="video/mp4"></video><video id="balasana" onended="balasanaClose()" width="1024" height="768" src="slide3/Balasana.mp4" type="video/mp4"></video><video id="konasana" onended="konasanaClose()" width="1024" height="768" src="slide3/Konasana.mp4" type="video/mp4"></video><audio id="correct" src="slide3/Correct.mp3" type="audio/mpeg"></audio><audio id="success" src="slide3/Success.mp3" type="audio/mpeg"></audio>';
-	break;
+
 }
 
 return content;
@@ -406,15 +363,13 @@ function showDiv2() {
    document.getElementById('welcomeDiv2').style.display = "block";
 }
 
-
 function open_page(url,page_id){
 	 // alert(page_id);
 	//step 10:
 	if (typeof(localStorage.getItem("currentslide"))!='undefined'){
 		//to checked previous slide has god end time...
 		var slideid=localStorage.getItem("currentslide");
-		toCaptureTime(slideid);
-		
+		toCaptureTime(slideid);	
 	}
 	
 	// toCaptureTime(page_id);
@@ -466,7 +421,6 @@ function open_page(url,page_id){
     if(currentslide == 2){
 	document.getElementById("click_through").innerHTML='';
 		}
-
 	}
 
 	function checkBtns(refNum){
@@ -523,6 +477,8 @@ function currentTimeInDatabaseFormat(){//to get current time in dd-mm-yyyy hh:mm
 	return duration;
 }
 
+// new js
+
 $(document).ready(function(){
 	$('body').on('click','.touchbtn',function(){
 		$('.right_arrow').trigger( "click" );
@@ -534,183 +490,29 @@ $(document).ready(function(){
 	})
 })
 
-
-
-/*--------------------- animation javascript -----------------------*/
-
-function pop_open() {
-	$('.popup').css("display","block");
-	$('.pop_close').css("display","block");
-	$('.pop_open').css("display","none");
-}
-
-function pop_close() {
-	$('.popup').css("display","none");
-	$('.pop_close').css("display","none");
-	$('.pop_open').css("display","block");
-}
-
-
 /*--------------------------Javascript Animation-----------------------------*/
 
-function halsanaHit() {	
-	$('.halsana').css("display","block");
-	$(".halsana").addClass("selected");
-	
-	document.getElementById("correct").play();
-	vid = document.getElementById("halsana");
-	vid.currentTime= 0;
-	vid.play();
-	$('#halsana').css("display","block");
+function playVid1() {
+	document.getElementById("startVideo").play();
+	$('.video1').css("display","block");
+	$('.button1').css("display","none");
 }
 
-function halsanaClose() {	
-	$('#halsana').css("display","none");
-	$('.halsanaHit').css("display","none");
-	
-	if ($(".konasana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".savasana").hasClass("selected") ||
-	   $(".konasana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".balasana").hasClass("selected") ||
-	   $(".konasana").hasClass("selected") && $(".savasana").hasClass("selected") && $(".balasana").hasClass("selected") ||
-	   $(".savasana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".balasana").hasClass("selected"))
-	{
-		$('.balasanaHit, .virasanaHit, .savasanaHit, .halsanaHit, .konasanaHit').css("display","none");
-		setTimeout(function() {
-			document.getElementById("success").play();
-			$('.score').css("display","block");
-		}, 1000);
-	}
-}
+function myFunction() {
+	$('.button2').css("display","block");
+};
 
+function playVid2() {
+	$('.button2').css("display","none");
+	$(".video1").css("display","none");
+	var vid = document.getElementById("startVideo");
+	vid.pause();
+	vid.currentTime = 0;
+	$(".video2").css("display","block");
+	document.getElementById("endVideo").play();
+};
 
-
-function virasanaHit() {	
-	$('.virasana').css("display","block");
-	$(".virasana").addClass("selected");
-	
-	document.getElementById("correct").play();
-	vid = document.getElementById("virasana");
-	vid.currentTime= 0;
-	vid.play();
-	$('#virasana').css("display","block");
-}
-
-function virasanaClose() {	
-	$('#virasana').css("display","none");
-	$('.virasanaHit').css("display","none");
-	
-	if ($(".halsana").hasClass("selected") && $(".savasana").hasClass("selected") && $(".balasana").hasClass("selected") ||
-	   $(".halsana").hasClass("selected") && $(".savasana").hasClass("selected") && $(".konasana").hasClass("selected") ||
-	   $(".halsana").hasClass("selected") && $(".balasana").hasClass("selected") && $(".konasana").hasClass("selected") ||
-	   $(".balasana").hasClass("selected") && $(".savasana").hasClass("selected") && $(".konasana").hasClass("selected"))
-	{
-		$('.virasanaHit, .savasanaHit, .balasanaHit, .konasanaHit, .halsanaHit').css("display","none");
-		setTimeout(function() {
-			document.getElementById("success").play();
-			$('.score').css("display","block");
-		}, 1000);
-	}
-}
-
-
-
-function savasanaHit() {	
-	$('.savasana').css("display","block");
-	$(".savasana").addClass("selected");
-	
-	document.getElementById("correct").play();
-	vid = document.getElementById("savasana");
-	vid.currentTime= 0;
-	vid.play();
-	$('#savasana').css("display","block");
-}
-
-function savasanaClose() {	
-	$('#savasana').css("display","none");
-	$('.savasanaHit').css("display","none");
-	
-	if ($(".halsana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".balasana").hasClass("selected") ||
-	   $(".halsana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".konasana").hasClass("selected") ||
-	   $(".halsana").hasClass("selected") && $(".balasana").hasClass("selected") && $(".konasana").hasClass("selected") ||
-	   $(".balasana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".konasana").hasClass("selected"))
-	{
-		$('.savasanaHit, .virasanaHit, .balasanaHit, .konasanaHit, .halsanaHit').css("display","none");
-		setTimeout(function() {
-			document.getElementById("success").play();
-			$('.score').css("display","block");
-		}, 1000);
-	}
-}
-
-
-
-function balasanaHit() {	
-	$('.balasana').css("display","block");
-	$(".balasana").addClass("selected");
-
-	document.getElementById("correct").play();
-	vid = document.getElementById("balasana");
-	vid.currentTime= 0;
-	vid.play();
-	$('#balasana').css("display","block");
-}
-
-function balasanaClose() {	
-	$('#balasana').css("display","none");
-	$('.balasanaHit').css("display","none");
-	
-	if ($(".halsana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".savasana").hasClass("selected") ||
-	   $(".halsana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".konasana").hasClass("selected") ||
-	   $(".halsana").hasClass("selected") && $(".savasana").hasClass("selected") && $(".konasana").hasClass("selected") ||
-	   $(".savasana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".konasana").hasClass("selected"))
-	{
-		$('.balasanaHit, .virasanaHit, .savasanaHit, .konasanaHit, .halsanaHit').css("display","none");
-		setTimeout(function() {
-			document.getElementById("success").play();
-			$('.score').css("display","block");
-		}, 1000);
-	}
-}
-
-
-
-function konasanaHit() {	
-	$('.konasana').css("display","block");
-	$(".konasana").addClass("selected");
-	
-	document.getElementById("correct").play();
-	vid = document.getElementById("konasana");
-	vid.currentTime= 0;
-	vid.play();
-	$('#konasana').css("display","block");
-}
-
-function konasanaClose() {	
-	$('#konasana').css("display","none");
-	$('.konasanaHit').css("display","none");
-	
-	if ($(".halsana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".savasana").hasClass("selected") ||
-	   $(".halsana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".balasana").hasClass("selected") ||
-	   $(".halsana").hasClass("selected") && $(".savasana").hasClass("selected") && $(".balasana").hasClass("selected") ||
-	   $(".savasana").hasClass("selected") && $(".virasana").hasClass("selected") && $(".balasana").hasClass("selected"))
-	{
-		$('.balasanaHit, .virasanaHit, .savasanaHit, .konasanaHit, .halsanaHit').css("display","none");
-		setTimeout(function() {
-			document.getElementById("success").play();
-			$('.score').css("display","block");
-		}, 1000);
-	}
-}
-
-
-
-function score() {	
-	$('.fact').css("display","block");
-}
-
-function fact() {	
-	$('.yoga').css("display","block");
-}
-
-function yoga() {	
-	$('.telma').css("display","block");
-}
+function myFunction1() {
+	document.getElementById("success").play();
+	$('.success').css("display","block");
+};
