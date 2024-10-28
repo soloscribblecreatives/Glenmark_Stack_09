81d37cfbe61b7dd34a35e0768979574224096f25
soloscribblecreatives <34957574+soloscribblecreatives@users.noreply.github.com>
2024-10-28 19:38:00 +0530

    Update

--- global.js
+++ global.js
@@ -347,7 +347,7 @@ currentSlide();
 var selectedContentPath='';
 switch(pg_id){
 	case 1:
-	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="s1"><img src="slide1/s1.png" width="1024" height="768" alt=""/></div><div class="s2"><img src="slide1/s2.png"/></div><div class="s3"><img src="slide1/s3.png"/></div><div class="s4"><img src="slide1/s4.png"/></div><div class="s5"><img src="slide1/s5.png"/></div><div class="s6wrap"><div class="s6"><img src="slide1/s6.png"/></div></div><div class="s7"><img src="slide1/s7.png"/></div><div class="s8wrap"><div class="s8"><img src="slide1/s8.png"/></div><div class="s9"><img src="slide1/s9.png"/></div><div class="s10"><img src="slide1/s10.png"/></div><div class="s11"><img src="slide1/s11.png"/></div></div><div class="s12"><img src="slide1/s12.png"/></div><div class="s13"><img src="slide1/s13.png"/></div><div class="s14"><img src="slide1/s14.png"/></div><div class="s15"><img src="slide1/s15.png"/></div><div class="s16"><img src="slide1/s16.png"/></div><div class="s17wrap"><div class="s17"><img src="slide1/s17.png"/></div><div class="s18"><img src="slide1/s18.png"/></div></div><div class="s19"><img src="slide1/s19.png"/></div><div class="s20"><img src="slide1/s20.png"/></div><div class="logo"><img src="slide1/logo.png"/></div><div class="lightUp" onclick="lightUp()"></div><div class="showGreet" onclick="showGreet()"></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="s1"><img src="slide1/s1.png" width="1024" height="768" alt=""/></div><div class="s2"><img src="slide1/s2.png"/></div><div class="s3"><img src="slide1/s3.png"/></div><div class="s4"><img src="slide1/s4.png"/></div><div class="s5"><img src="slide1/s5.png"/></div><div class="s6wrap"><div class="s6"><img src="slide1/s6.png"/></div></div><div class="s7"><img src="slide1/s7.png"/></div><div class="s8wrap"><div class="s8"><img src="slide1/s8.png"/></div><div class="s9"><img src="slide1/s9.png"/></div><div class="s10"><img src="slide1/s10.png"/></div><div class="s11"><img src="slide1/s11.png"/></div></div><div class="s12"><img src="slide1/s12.png"/></div><div class="s13"><img src="slide1/s13.png"/></div><div class="s14"><img src="slide1/s14.png"/></div><div class="s15"><img src="slide1/s15.png"/></div><div class="s16"><img src="slide1/s16.png"/></div><div class="s17wrap"><div class="s17"><img src="slide1/s17.png"/></div><div class="s18"><img src="slide1/s18.png"/></div></div><div class="s19"><img src="slide1/s19.png"/></div><div class="s20"><img src="slide1/s20.png"/></div><div class="logo"><img src="slide1/logo.png"/></div><div class="lightUp" onclick="lightUp()"></div>';
 	break;
 
 }
@@ -503,11 +503,6 @@ function lightUp() {
 	$('.s9').css("display","block");
 	$('.s10').css("display","block");
 	$('.s11').css("display","block");
-	$('.showGreet').css("display","block");
-}
-
-function showGreet() {
-	$('.showGreet').css("display","none");
 	$('.s12').css("display","block");
 	$('.s13').css("display","block");
 	$('.s14').css("display","block");
